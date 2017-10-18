a8a191c09c837a33dbd91a0c835ef424302a05c7
arunkumarthiags <arunkumarthiags@gmail.com>
2017-10-18 07:20:26 -0700

    Deleting this file

--- cp1251-character-detection.sql
+++
@@ -1,2 +0,0 @@
-select MESSAGE_ID, count(*) from mailing_list_message_table
-where MESSAGE_TEXT_AOL LIKE concat('%',X'8F','%') or MESSAGE_TEXT_AOL LIKE concat('%',X'90','%') or MESSAGE_TEXT_AOL LIKE concat('%',X'81','%') or MESSAGE_TEXT_AOL LIKE concat('%',X'8D','%')or MESSAGE_TEXT_AOL LIKE concat('%',X'9D','%');
